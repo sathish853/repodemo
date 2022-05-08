006940dc23b4667142469e8d14e578d09f2c6af0
sathish <sathish853@gmail.com>
2022-05-08 21:26:10 +0530

    updated a minor code
    minor code

---
+++ JavaPractice/src/test/java/Java/Demo/JavaPractice/javapractie.java
@@ -0,0 +1,9 @@
+package Java.Demo.JavaPractice;
+
+public class javapractie {
+public static void main() {
+	System.out.println("i am sathish");
+	System.out.println("update on git");
+	System.out.println("now");
+}
+}
